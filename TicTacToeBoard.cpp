5fef0beb86bbfd197530301384fa4768f82addc0
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-09-20 09:01:47 +0000

    implemented TicTacToeBoard functions + intended bug

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -19,7 +19,14 @@ TicTacToeBoard::TicTacToeBoard()
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+  if(turn == X) {
+		turn = O;
+		return turn;
+	} 
+	else {
+		turn = X;
+		return turn;
+	} 
 }
 
 /**
@@ -33,7 +40,17 @@ Piece TicTacToeBoard::toggleTurn()
 **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
-  return Invalid;
+  if(row > 2 || column > 2){
+    return Invalid;
+  }
+  if(getPiece(row, column) == Blank){
+    board[row][column] = turn;
+  }
+  if(getWinner() == (X || O)){
+    return board[row][column];
+  }
+  toggleTurn();
+  return board[row][column];
 }
 
 /**
@@ -42,7 +59,10 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
-  return Invalid;
+  if(row > 2 || column > 2){
+    return Invalid;
+  }
+  return board[row][column];
 }
 
 /**
@@ -51,5 +71,28 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+  for(int i=0; i<BOARDSIZE; i++){
+    for(int j=0; j<BOARDSIZE; j++){
+      if(board[i][j] == Blank){
+        return Invalid;
+      }
+    }
+  }
+  if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+    return board[0][0];
+  }else if(board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+		return board[0][2];
+	} else if(board[0][0] == board[0][1] && board[0][0] == board[0][2]) {
+		return board[0][0];
+	} else if(board[1][0] == board[1][1] && board[1][0] == board[1][2]) {
+		return board[1][0];
+	} else if(board[2][0] == board[2][1] && board[2][0] == board[2][2]) {
+		return board[2][0];
+	} else if(board[0][0] == board[1][0] && board[0][0] == board[2][0]) {
+		return board[0][0];
+	} else if(board[0][1] == board[1][1] && board[0][1] == board[2][1]) {
+		return board[0][1];
+	} else if(board[0][2] == board[1][2] && board[0][2] == board[2][2]) {
+		return board[0][2];
+	return Blank;
 }
